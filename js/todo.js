40a1ce0fdf81b1738d4a879b560ed6c9502f194a
Khalid Sookia <khalidsookia@gmail.com>
2015-09-24 01:46:30 +0200

    Initialised app and added blank controller

---
+++ js/todo.js
@@ -0,0 +1,11 @@
+/**
+ * Instatiation du module todoApp. C'est ici que angular cherchera le code a exécuter
+ *  pour notre application todoApp puisque c'est le nom qu'on lui a donné dans notre directive
+ */
+angular.module('todoApp', []);
+
+/**
+ * Chainage du controller au module avec le même nom qu'on lui a donné dans notre vue.
+ */
+todoApp.controller('TodoListController', function() {
+});
