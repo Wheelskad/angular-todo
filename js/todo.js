a6e1dfba7caa26f82852b40647d37d5f5babaf2e
Khalid Sookia <khalidsookia@gmail.com>
2015-09-24 02:32:11 +0200

    Added in js the todo array and in html the corresponding directives for injecting the todos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,26 @@
  * Instatiation du module todoApp. C'est ici que angular cherchera le code a exécuter
  *  pour notre application todoApp puisque c'est le nom qu'on lui a donné dans notre directive
  */
-angular.module('todoApp', []);
+var todoApp = angular.module('todoApp', []);
 
 /**
  * Chainage du controller au module avec le même nom qu'on lui a donné dans notre vue.
  */
-todoApp.controller('TodoListController', function() {
+todoApp.controller('TodoListController', function($scope) {
+
+    //Instanciation du todoList avec la valeur $scope
+    var todoList = $scope;
+
+    // Ajout de la propriété todos à todoList
+    // Le todos est un tableau contenant des objets todos
+    todoList.todos = [
+        {
+            text : 'Learn javascript',
+            done : false
+        },
+        {
+            text : 'Learn angular',
+            done : false
+        }
+    ];
 });
